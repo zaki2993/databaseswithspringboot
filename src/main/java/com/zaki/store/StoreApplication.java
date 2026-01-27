3b42101e947e624f8d6a0f6be406e7783fc214ce
zakaryae bouabdallah <zakaprog@gmail.com>
2026-01-27 17:12:04 +0100

    simplifying code with lambok

--- src/main/java/com/zaki/store/StoreApplication.java
+++ src/main/java/com/zaki/store/StoreApplication.java
@@ -3,11 +3,15 @@ package com.zaki.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.zaki.store.entities.User;
+
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		// SpringApplication.run(StoreApplication.class, args);
+    User u = new User(1L,"zakaryae","zakaryae@gmail","zakaps");
+    System.out.println(u.getName());
 	}
 
 }
--- src/main/java/com/zaki/store/entities/User.java
+++ src/main/java/com/zaki/store/entities/User.java
@@ -6,8 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Setter
+@Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
   @Id
@@ -19,37 +26,5 @@ public class User {
   private String email;
   @Column(name = "password")
   private String password;
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
- public long getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
 
 }
