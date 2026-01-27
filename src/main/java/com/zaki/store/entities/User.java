c3b5d30fc48c91588418f1692a22d1c134348f54
zakaryae bouabdallah <zakaprog@gmail.com>
2026-01-27 13:41:54 +0100

    using entities

---
+++ src/main/java/com/zaki/store/entities/User.java
@@ -0,0 +1,55 @@
+package com.zaki.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  @Column(name = "name")
+  private String name;
+  @Column(name = "email")
+  private String email;
+  @Column(name = "password")
+  private String password;
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+ public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+}
